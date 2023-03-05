66877a8a803f30568e1b20918b05b8e0faf6e18e
Balantaim <martin.ned.atanasov@gmail.com>
2023-03-05 16:16:14 +0200

    Empty Project - first init

--- app/src/main/java/com/rainbowinfinium/invasion/MainActivity.kt
+++ app/src/main/java/com/rainbowinfinium/invasion/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.rainbowinfinium.invasion
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -16,17 +20,51 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.rainbowinfinium.invasion.ui.theme.InvasionTheme
 
+
+
 class MainActivity : ComponentActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             InvasionTheme {
                 // A surface container using the 'background' color from the theme
+                setFullscreen(window)
+//                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+//                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
+//                window.decorView.apply {
+//                    // Hide both the navigation bar and the status bar.
+//                    // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+//                    // a general rule, you should design your app to hide the status bar whenever you
+//                    // hide the navigation bar.
+//                    systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+//                }
+
+
+
                 UserField()
             }
         }
+    }
+}
+
+private fun setFullscreen(window: Window) {
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+    WindowInsetsControllerCompat(window,
+        window.decorView.findViewById(android.R.id.content)).let { controller ->
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+
+        // When the screen is swiped up at the bottom
+        // of the application, the navigationBar shall
+        // appear for some time
+        controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 }
 
